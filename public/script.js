a7027724d319206a6a565fd340ba9363849087b9
biv@vitronic.com <biv@vitronic.com>
2025-07-12 14:04:07 +0200

    Add basic user message exchange

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
 const socket = io();
+
+const userEl = document.getElementById("user");
+const textEl = document.getElementById("text");
+const chatBoxEl = document.getElementById("chat-box");
+
+socket.on("send message", (msg) => {
+  const pEl = document.createElement("p");
+  pEl.textContent = `${msg.username}: ${msg.message}`;
+  chatBoxEl.appendChild(pEl);
+  chatBoxEl.scrollTop = chatBoxEl.scrollHeight;
+});
+
+function sendMessage() {
+  const userName = userEl.value;
+  const message = textEl.value.trim();
+  if (!userName || !message) return;
+
+  const payload = {
+    username: userName,
+    message: message,
+  };
+
+  socket.emit("send message", payload);
+
+  textEl.value = "";
+}
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const http = require('node:http');
-const { Server } = require('socket.io');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const http = require("node:http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,15 +11,22 @@ const PORT = 3000;
 const io = new Server(server);
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-io.on('connection', (socket) => {
-   
-    console.log('User connected:', socket.id);
-  
+  socket.on("send message", (msg) => {
+    console.log(msg);
+
+    io.emit("send message", {
+      username: msg.username,
+      message: msg.message,
+    });
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
